5ca43dfc045883d4b52a137c3eeabef094720668
mikemd833 <mdiep044@uottawa.ca>
2020-03-25 08:37:15 -0400

    applied Grid Layout to Chat Page

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,15 +1,56 @@
 import React from 'react';
+//Layout Imports
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = (theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paperTitle: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+    paperLeft: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        height: 730,
+    },
+    paperRight: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        height: 730,
+    },
+    media: {
+        height: 400,
+      },
+  });
 
 class ChatPage extends React.Component {
     render() {
+        const { classes } = this.props;
+
         return (
             <div>
-                <p>
-                    Chat
-                </p>
+                <Grid container spacing={3}>
+                    <Grid item xs={3}>
+                        <Paper className={classes.paperLeft}>
+
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={9}>
+                        <Paper className={classes.paperRight}>
+
+                        </Paper>
+                    </Grid>
+                </Grid>
             </div>
         );
     }
 }
 
-export default ChatPage;
+export default withStyles(styles, {withTheme: true})(ChatPage);
