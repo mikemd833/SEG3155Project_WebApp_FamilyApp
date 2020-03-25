5c1e5bb783fe157696bf06825b812f8cdb4ffb92
mikemd833 <mdiep044@uottawa.ca>
2020-03-25 09:50:19 -0400

    Added layout for family member selection in chat

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,23 +1,26 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+
 //Layout Imports
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = (theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    paperTitle: {
+    mainPaperLeft: {
         padding: theme.spacing(2),
         textAlign: 'center',
         color: theme.palette.text.secondary,
+        maxHeight: 730,
     },
-    paperLeft: {
+    subPaperLeft: {
         padding: theme.spacing(2),
         textAlign: 'center',
         color: theme.palette.text.secondary,
-        height: 730,
+        overflow: 'auto',
+        maxHeight: 660,
     },
     paperRight: {
         padding: theme.spacing(2),
@@ -25,9 +28,16 @@ const styles = (theme) => ({
         color: theme.palette.text.secondary,
         height: 730,
     },
-    media: {
-        height: 400,
-      },
+    chatButton: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        background: '#2699FB',
+        height: 100,
+        width: 380
+    },
+    gridListItem: {
+        padding: theme.spacing(1),
+    }
   });
 
 class ChatPage extends React.Component {
@@ -37,11 +47,37 @@ class ChatPage extends React.Component {
         return (
             <div>
                 <Grid container spacing={3}>
+                    {/* Left Side */}
                     <Grid item xs={3}>
-                        <Paper className={classes.paperLeft}>
-
+                        <Paper className={classes.mainPaperLeft}>
+                            <Typography variant="h5" component="h2">Chat Rooms</Typography>
+                            <Paper className={classes.subPaperLeft}>
+                                {/* Each Chat */}
+                                    <Grid item xs={12} className={classes.gridListItem}>
+                                        <Button className={classes.chatButton}> Family Chat </Button>
+                                    </Grid>
+                                    <Grid item xs={12} className={classes.gridListItem}>
+                                        <Button className={classes.chatButton}> Karim </Button>
+                                    </Grid>
+                                    <Grid item xs={12} className={classes.gridListItem}>
+                                        <Button className={classes.chatButton}> Matt </Button>
+                                    </Grid>
+                                    <Grid item xs={12} className={classes.gridListItem}>
+                                        <Button className={classes.chatButton}> Mike </Button>
+                                    </Grid>
+                                    <Grid item xs={12} className={classes.gridListItem}>
+                                        <Button className={classes.chatButton}> Miguel </Button>
+                                    </Grid>
+                                    <Grid item xs={12} className={classes.gridListItem}>
+                                        <Button className={classes.chatButton}> Alan </Button>
+                                    </Grid>
+                                    <Grid item xs={12} className={classes.gridListItem}>
+                                        <Button className={classes.chatButton}> Tanner </Button>
+                                    </Grid>
+                            </Paper>
                         </Paper>
                     </Grid>
+                    {/* Right Side */}
                     <Grid item xs={9}>
                         <Paper className={classes.paperRight}>
 
